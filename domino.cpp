86e169310ae6a37fcef27c2d3b03d7eed20dd33a
Ararsa-Derese <116437655+Ararsa-Derese@users.noreply.github.com>
2022-10-23 11:48:56 +0300

    Create domino.cpp

---
+++ domino.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+ int main()
+{
+   int m, n;
+   cin>>m>>n;
+   int totalsize= m*n;
+   int maxdomino= totalsize/2;
+   cout<<maxdomino;
+
+    return 0;
+ }
