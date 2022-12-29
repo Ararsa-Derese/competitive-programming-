29d50c13245d81ab19fc46431390559977ed7edc
Ararsa-Derese <116437655+Ararsa-Derese@users.noreply.github.com>
2022-12-29 11:25:23 +0300

    Create minimizemaxpair.cpp

---
+++ minimizemaxpair.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        int max=0;
+        sort(nums.begin(), nums.end());
+        int i=0, j=nums.size()-1;
+        while(i<j){
+            if(nums[i]+nums[j]>max)
+            max=nums[i]+nums[j];
+            i++;
+            j--;
+        }
+        return max;
+    }
+};
