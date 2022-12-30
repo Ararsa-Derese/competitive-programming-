6014827f11416f85d7a7366cd3d6ef6080cc24db
Ararsa-Derese <116437655+Ararsa-Derese@users.noreply.github.com>
2022-12-30 19:25:31 +0300

    Create boatstosave.cpp

---
+++ boatstosave.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+         int i = 0, j = people.size() - 1,count = 0;
+      sort(people.begin(),people.end());
+        while(i <= j)
+        {   
+         if(people[i] + people[j] <= limit)
+            {
+            i++;
+                
+            }
+          
+                j--;
+            
+            count++;
+        }
+        
+        
+        
+        
+        return count; 
+    }
+};
