a25a533163b42257ce1187517439463736086368
Ararsa-Derese <116437655+Ararsa-Derese@users.noreply.github.com>
2023-03-21 16:22:14 +0300

    Time: 181 ms (41.78%), Space: 107.4 MB (86.43%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-   long long int fact(int c){
-        long long int sum=0;
-        while(c){
-            sum=sum+c;
-            c--;
-        }
-        return sum;
-    }
+  
     long long zeroFilledSubarray(vector<int>& nums) {
-        long long int c=0;
-            long long int sum=0;
+        double c=0;
+             double sum=0;
             for(long long int n: nums){
                 if (n==0)
                     c++;
                 else{
                     if (c!=0){
-                        sum=sum+(fact(c));
+                       sum=sum+((long long int)(((1+c)/2)*c));
                         c=0;
                         }
                     }
                 }
             if (c!=0)
-                 sum=sum+(fact(c));
+                sum=sum+((long long int)(((1+c)/2)*c));
         return sum;
     }
 };
