f9682af5d49f5a1884cadbc077332e51d1cef696
Ararsa-Derese <116437655+Ararsa-Derese@users.noreply.github.com>
2023-03-21 11:50:00 +0300

    Time: 1048 ms (93.99%), Space: 24.5 MB (100.00%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
@@ -1,12 +1,7 @@
 class Solution:
    
     def zeroFilledSubarray(self, nums: List[int]) -> int: 
-            def fact(c):
-                sum=0
-                while c:
-                   sum=sum+c
-                   c-=1
-                return sum
+          
             c=0
             sum=0
             for n in nums:
@@ -14,8 +9,8 @@ class Solution:
                     c+=1
                 else:
                     if c!=0:
-                        sum=sum+(fact(c))
+                        sum=sum+(int(((1+c)/2)*c))
                         c=0
             if c!=0:
-                 sum=sum+(fact(c))
+                 sum=sum+(int(((1+c)/2)*c))
             return sum
