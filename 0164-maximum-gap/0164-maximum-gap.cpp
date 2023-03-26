1bffff64bc69985fb02fc985482249203d7c88c6
Ararsa-Derese <116437655+Ararsa-Derese@users.noreply.github.com>
2023-03-26 23:19:58 +0300

    Time: 202 ms (83.54%), Space: 82.1 MB (70.85%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        if(nums.size()==1)
+            return 0;
+        int max=INT_MIN;
+       sort(nums.begin(),nums.end());
+       for(int i=1; i<nums.size(); i++){
+           if(nums[i]-nums[i-1]>max)
+               max=nums[i]-nums[i-1];
+       }
+      return max;
+    }
+};
