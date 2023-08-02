0eeb2935f6b8aaa5564b0724d12030feee8e7786
Ararsa <116437655+Ararsa-Derese@users.noreply.github.com>
2023-08-02 22:38:14 +0300

    Time: 148 ms (95.41%), Space: 17.9 MB (88.03%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,9 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for num in nums:
-            if num==0:
-                nums.remove(num)
-                nums.append(0)
-                
+        k=0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                temp=nums[i]
+                nums[i]=0
+                nums[k]=temp
+                k+=1
         return nums
